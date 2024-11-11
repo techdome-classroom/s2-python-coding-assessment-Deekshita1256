d2ca7bca08b4919c6471305ebc893e6f7872d00f
Deekshita1256 <deekshitadurgam.1256@gmail.com>
2024-11-11 15:24:21 +0530

    Passed all tests for program1 and program2

--- program1.py
+++ program1.py
@@ -1,20 +1,21 @@
 class Solution(object):
-    def isValid(self, s):
+    def isValid(self, st):
         """
         :type s: str
         :rtype: bool
         """
-        pass
+        bracket_map = {')': '(', ']': '[', '}': '{'}
+        stack = []
 
+        for char in st:
+           
+            if char in bracket_map:
+               
+                top_element = stack.pop() if stack else '#'
+                
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
 
-
-
-
-
-
-    
-
-
-
-  
-
+        return not stack
